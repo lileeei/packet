b7ce3ba6b6da3fc46214f62871f9a090858ba8fd
lileeei <lileiwell1993@163.com>
2016-10-26 23:06:15 +0800

    Update common.go

--- common.go
+++ common.go
@@ -1,233 +1,262 @@
 package packet
 
 import (
+	"errors"
 	"strconv"
+	"math"
 )
-
-type PlayLoad struct {
-	pos int32   	//偏移量
-	Data []byte	//数据
-}
 
 const (
 	INITLEN = 128
 )
 
-func NewPlayLoad() *PlayLoad {
-	return &PlayLoad{
-		pos: 0,
+
+//--------------------------------------------------------------------------分割线
+type Packet struct {
+	pos  int32  //偏移量
+	Data []byte //数据
+}
+
+func (pkt *Packet)Pos() int32 {
+	return pkt.pos
+}
+
+func (pkt *Packet)Seek(left int32) bool {
+	if pkt.pos + left >= len(pkt.Data) {
+		return false
+	}
+	
+	pkt.pos += left
+	
+	return true
+}
+
+func NewPlayLoad() *Packet {
+	return &Packet{
+		pos:  0,
 		Data: make([]byte, 0, INITLEN),
 	}
 }
 
-
 //---------------------------------------------------------------------------读取函数
 //读取固定数量个byte
-func (playload *PlayLoad)ReadByte(v... int) []byte {
+func (pkt *Packet) ReadByte(v ...int) (bytes []byte, err error) {
 	byteNum := append(v, 1)[0]
 
-	if len(playload.Data) < byteNum {
-		return nil
+	if len(pkt.Data) < byteNum {
+		return nil, errors.New("read byte error")
 	}
-	pos := playload.pos
-	playload.pos += int32(byteNum)
 
-	return playload.Data[pos:playload.pos]
+	pos := pkt.pos
+	pkt.pos += int32(byteNum)
+
+	return pkt.Data[pos:pkt.pos], nil
 }
 
-func (playload *PlayLoad)ReadBool() bool {
-	bt := playload.ReadByte()
-	//if bt == nil {
-	//	return nil
-	//}
+func (pkt *Packet) ReadBool() (value bool,err error) {
+	bt, err := pkt.ReadByte()
+	if err != nil {
+		err = errors.New("read bool error")
+		return
+	}
 
-	return bt[0] == byte(1)
+	return bt[0] == byte(1), nil
 }
 
-func (playload *PlayLoad)ReadInt8() int8 {
-	data := playload.ReadByte()
-	//if Data == nil{
-	//	return Data
-	//}
+func (pkt *Packet) ReadInt8() (value int8, err error) {
+	data, err := pkt.ReadByte()
+	if err != nil {
+		err = errors.New("read int8 error")
+		return
+	}
 
-	return int8(data[0])
+	return int8(data[0]), nil
 }
 
-func (playload *PlayLoad)ReadUint8() uint8 {
-	data := playload.ReadByte()
-	//if Data == nil{
-	//	return Data
-	//}
+func (pkt *Packet) ReadUint8() (value uint8, err error) {
+	data, err := pkt.ReadByte()
+	if err != nil {
+		err = errors.New("read uint8 error")
+		return
+	}
 
 	return uint8(data[0])
 }
 
-func (playload *PlayLoad)ReadInt16() int16{
-	bt := playload.ReadByte(2)
+func (pkt *Packet) ReadInt16() (value int16, err error) {
+	data, err := pkt.ReadByte(2)
+	if err != nil {
+		err = errors.New("read int16 error")
+		return
+	}
 
-	value, _ := strconv.ParseInt(string(bt), 10, 16)
-	//if err != nil {
-	//	return nil
-	//}
-	return int16(value)
+	value = int16( data[0]<<8 ) | int16(data[1])
+
+	return
 }
 
+func (pkt *Packet) ReadUint16() (value uint16, err error) {
+	data, err := pkt.ReadByte(2)
+	if err != nil {
+		err = errors.New("read uint16 error")
+		return
+	}
 
-func (playload *PlayLoad)ReadUint16() uint16{
-	bt := playload.ReadByte(2)
+	value = uint16( data[1]<<8 ) | uint16(data[0])
 
-	value, _ := strconv.ParseUint(string(bt), 10, 16)
-	//if err != nil {
-	//	return nil
-	//}
-	return uint16(value)
+	return
 }
 
+func (pkt *Packet) ReadInt32() (value int32, err error) {
+	data, err := pkt.ReadByte(4)
+	if err != nil {
+		err = errors.New("read int32 error")
+		return
+	}
 
-func (playload *PlayLoad)ReadInt32() int32{
-	bt := playload.ReadByte(4)
+	for k, v := range data {
+		value |= int32(v<<(3-k)*8)
+	}
 
-	value, _ := strconv.ParseInt(string(bt), 10, 32)
-	//if err != nil {
-	//	return nil
-	//}
-
-	return int32(value)
+	return
 }
 
+func (pkt *Packet) ReadUint32() (value uint32, err error) {
+	data, err := pkt.ReadByte(4)
+	if err != nil {
+		err = errors.New("read uint32 error")
+		return
+	}
 
-func (playload *PlayLoad)ReadUint32() uint32{
-	bt := playload.ReadByte(4)
+	for k, v := range data {
+		value |= uint32(v<<(3-k)*8)
+	}
 
-	value, _ := strconv.ParseUint(string(bt), 10, 32)
-	//if err != nil {
-	//	return nil
-	//}
 
-	return uint32(value)
+	return
 }
 
-func (playload *PlayLoad)ReadInt64() int64{
-	bt := playload.ReadByte(8)
+func (pkt *Packet) ReadInt64() (value int64, err error) {
+	data, err := pkt.ReadByte(8)
+	if err != nil {
+		err = errors.New("read int64 error")
+		return
+	}
 
-	value, _ := strconv.ParseInt(string(bt), 10, 64)
-	//if err != nil {
-	//	return nil
-	//}
+	for k, v := range data {
+		value |= int64(v<<(7-k)*8)
+	}
 
-	return int64(value)
+	return
 }
 
-func (playload *PlayLoad)ReadUint64() uint64{
-	bt := playload.ReadByte(8)
+func (pkt *Packet) ReadUint64() (value uint64, err error) {
+	data, err := pkt.ReadByte(8)
+	if err != nil {
+		err = errors.New("read uint64 error")
+		return
+	}
 
-	value, _ := strconv.ParseUint(string(bt), 10, 64)
-	//if err != nil {
-	//	return nil
-	//}
+	for k, v := range data {
+		value |= int64(v<<(7-k)*8)
+	}
 
-	return uint64(value)
+	return
 }
 
-func (playload *PlayLoad)ReadFloat32() float32 {
-	bt := playload.ReadByte(4)
+func (pkt *Packet) ReadFloat32() (value float32, err error) {
+	data, err := pkt.ReadUint32()
+	if err != nil {
+		err = errors.New("read float32 error")
+		return
+	}
 
-	value, _ := strconv.ParseFloat(string(bt), 32)
-	//if err != nil {
-	//	return
-	//}
+	value = math.Float32frombits(data)
 
-	return float32(value)
+	return
 }
 
-func (playload *PlayLoad)ReadString(v... int) string{
-	leftLen := len(playload.Data) - int(playload.pos)
-	num := append(v, leftLen)[0]
+func (pkt *Packet) ReadString(v ...int) (value float64, err error) {
+	data, err := pkt.ReadUint64()
+	if err != nil {
+		err = errors.New("read float64 error")
+		return
+	}
 
-	bt := playload.ReadByte(num)
-	//if bt != nil {
-	//	return string(bt)
-	//}
+	value = math.Float32frombits(data)
 
-	return string(bt)
+	return
 }
-
-
-
 
 //-----------------------------------------------------------------------写入函数
 
-func (playload *PlayLoad)WriteBytes(elems... byte) {
-	var index int = 0
-
-	//for playload.pos < int32(cap(playload.Data)) && index < len(elems) {
-	//	playload.Data[playload.pos] = elems[index]
-	//	playload.pos++
-	//	index++
-	//}
-	//
-	//if index < len(elems) {
-		playload.Data = append(playload.Data, elems[index:]...)
-	//}
+func (pkt *Packet) WriteBytes(elems []byte) {
+	pkt.Data = append(pkt.Data, elems[0:]...)
 }
 
-func (playload *PlayLoad)WriteBool(elem bool) {
+func (pkt *Packet) WriteBool(elem bool) {
 	if elem {
-		playload.WriteBytes(byte(1))
+		pkt.WriteBytes(byte(1))
 	} else {
-		playload.WriteBytes(byte(0))
+		pkt.WriteBytes(byte(0))
 	}
 }
 
-func (playload *PlayLoad)WriteInt8(elem int8)  {
-	playload.WriteBytes(byte(elem))
+func (pkt *Packet) WriteInt8(elem int8) {
+	pkt.WriteBytes(byte(elem))
 }
 
-func (playload *PlayLoad)WriteUint8(elem uint8) {
-	playload.WriteBytes(byte(elem))
+func (pkt *Packet) WriteUint8(elem uint8) {
+	pkt.WriteBytes(byte(elem))
 }
 
-func (playload *PlayLoad)WriteInt16(elem int16) {
-	playload.WriteBytes([]byte(string(elem))...)
+func (pkt *Packet) WriteUint16(elem int16) {
+	bt := make([]byte, 2)
+	bt[0] = byte(elem >> 8)
+	bt[1] = byte(elem)
+
+	pkt.WriteBytes(bt)
 }
 
-
-func (playload *PlayLoad)WriteUint16(elem int16) {
-	playload.WriteBytes([]byte(string(elem))...)
+func (pkt *Packet) WriteInt16(elem int16) {
+	pkt.WriteUint16(uint16(elem))
 }
 
+func (pkt *Packet) WriteUint32(elem uint32) {
+	bt := make([]byte, 4)
+	bt[0] = byte(elem >> 24)
+	bt[1] = byte(elem >> 16)
+	bt[2] = byte(elem >> 8)
+	bt[3] = byte(elem)
 
-func (playload *PlayLoad)WriteInt32(elem int32) {
-	playload.WriteBytes([]byte(string(elem))...)
+	pkt.WriteBytes(bt)
 }
 
-
-func (playload *PlayLoad)WriteUint32(elem uint32) {
-	playload.WriteBytes([]byte(string(elem))...)
+func (pkt *Packet) WriteInt32(elem int32) {
+	pkt.WriteUint32(uint32(elem))
 }
 
-func (playload *PlayLoad)WriteInt64(elem int64) {
-	playload.WriteBytes([]byte(string(elem))...)
+func (pkt *Packet) WriteUint64(elem uint64) {
+	bt := make([]byte, 8)
+	for i := 0; i < 8; i++ {
+		bt[i] = byte(elem >> (7-1)*8)
+	}
+
+	pkt.WriteBytes(bt)
 }
 
-func (playload *PlayLoad)WriteUint64(elem uint64){
-	playload.WriteBytes([]byte(string(elem))...)
+func (pkt *Packet) WriteInt64(elem int64) {
+	pkt.WriteInt64(uint64(elem))
 }
 
-func (playload *PlayLoad)WriteFloat32(elem float32) {
-	s := strconv.FormatFloat(float64(elem), 'f', -1, 32)
-	playload.WriteBytes([]byte(s)...)
+func (pkt *Packet) WriteFloat32(elem float32) {
+	pkt.WriteUint32(math.Float32bits(elem))
 }
 
-func (playload *PlayLoad)WriteFloat64(elem float64) {
-	s := strconv.FormatFloat(elem, 'f', -1, 64)
-	playload.WriteBytes([]byte(s)...)
+func (pkt *Packet) WriteFloat64(elem float64) {
+	pkt.WriteUint64(math.Float64bits(elem))
 }
 
-func (playload *PlayLoad)WriteString(elem string) {
-	playload.WriteBytes([]byte(elem)...)
+func (playload *Packet) WriteString(elem string) {
+	playload.WriteBytes([]byte(elem))
 }
-
-
-
-
