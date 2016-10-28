52c74f74263321e43fd98d40c4e091ed51601f30
lileeei <lileiwell1993@163.com>
2016-10-28 15:45:45 +0800

    Update pack.go

--- pack.go
+++ pack.go
@@ -2,10 +2,14 @@ package packet
 
 
 func Pack(value interface{}) (bytes []byte, err error) {
+	valueOf := reflect.ValueOf(value)
 	//todo
+	return 
 }
 
 
 func UnPack() error {
 	//todo
+	
+	return 
 }
